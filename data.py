373c84aaa284fb5fada00c025352eb8416732214
ChingisBadmaev <chingis.badmaev2018@yandex.ru>
2019-10-17 18:59:00 +0300

    стайлкод + приветствие

--- data.py
+++ data.py
@@ -1,6 +1,11 @@
+
 def get_token():
     return '966505306:AAHFTinGFcnYjZ7abFCZG9wAO47OkkAqkhI'
+
+
 def sasha_chat_id():
     return 260009462
+
+
 def get_proxy():
     return 'https://157.245.0.181:3128'
--- main.py
+++ main.py
@@ -1,35 +1,31 @@
+
 import telebot
 from telebot import apihelper
-    #файл для констант
 import data
-    #файл, в котором будут собраны функции для работы с базой данных
-import bd_def
-    #proxy
-apihelper.proxy = {'https': data.get_proxy()}
-    #инициализация бота
-bot = telebot.TeleBot(data.get_token())
+import bd_def  # файл, в котором будут собраны функции для работы с базой данных
 
-    #распознование команды /start
-@bot.message_handler(commands=['start'])
+apihelper.proxy = {'https': data.get_proxy()}  # proxy
+bot = telebot.TeleBot(data.get_token())  # инициализация бота
+
+name = ''
+@bot.message_handler(commands=['start'])  # распознование команды /start
 def start_message(message):
-        #отправка сообщения пользователю
-    bot.send_message(message.chat.id, 'Приветствую')
-        #получение данных о пользователе
-    user_id = message.chat.id
+    bot.send_message(message.chat.id, 'Приветствую')  # отправка сообщения пользователю
+    user_id = message.chat.id  # получение данных о пользователе
     username = str(message.chat.username)
     firstname = str(message.chat.first_name)
     secondname = str(message.chat.last_name)
-        #добавление пользователя в базу данных
-    bd_def.add_user(user_id,username,firstname,secondname)
+    bd_def.add_user(user_id, username, firstname, secondname)  # добавление пользователя в базу данных
+    bot.send_message(message.from_user.id, 'Как тебя зовут?')  # отправка сообщения пользователю
+    bot.register_next_step_handler(message, get_name)  # следующий шаг – функция get_name
 
-    #распознование текстового сообщения
-@bot.message_handler(content_types=['text'])
-def send_message(message):
-    bot.send_message(message.chat.id, 'Kekс')
-        #вывод сообщения в консоль pycharm
-    print(message.chat.username + ' : ' + message.text )
-        #отправка сообщения Саше о том, что боту кто-то написал
-    bot.send_message(data.sasha_chat_id(),message.chat.username + ' : ' + message.text)
 
-    #это метод, чтобы бот не завершался
+def get_name(message):  # вывод сообщения
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Хорошо, я запомню, что тебя зовут ' + name)
+    print(message.chat.first_name + ' : ' + message.text)  # вывод сообщения в консоль pycharm
+    bot.send_message(data.sasha_chat_id(), message.chat.first_name + ' : ' + message.text)  # сбщ, что боту написали
+
+
 bot.polling()
