07be56ca22c99346a09e720313fcf75b77be2399
Aleksandr <alex.sergeevich1348@gmail.com>
2019-10-17 13:22:23 +0300

    addcomments

--- bd_def.py
+++ bd_def.py
@@ -1,9 +1,17 @@
+    #библиотека для работы с базой данных
 import sqlite3
+
+    #добавление пользователя в базу данных
 def add_user(user_id,username,firstname,secondname):
+        #подключение к базе данных
     con = sqlite3.connect('./database.db')
     cur = con.cursor()
+        #создание таблицы, если она еще не создана
     cur.execute('CREATE TABLE IF NOT EXISTS users(user_id INT, username TEXT, firstname TEXT, secondname TEXT, UNIQUE(user_id,username,firstname,secondname))')
+        #добавление пользователя в таблицу
     cur.execute('INSERT OR IGNORE INTO users VALUES(' + str(user_id) + ',"' + username + '", "' + firstname + '", "' + secondname + '")')
+        #сохранение изменений в базе данных
     con.commit()
+        #отключение от базы данных
     cur.close()
     con.close()
--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import telebot
 from telebot import apihelper
+    #файл для констант
 import data
+    #файл, в котором будут собраны функции для работы с базой данных
 import bd_def
-#apihelper.proxy = {'https': data.get_proxy()}
-
-
+    #proxy
+apihelper.proxy = {'https': data.get_proxy()}
+    #инициализация бота
 bot = telebot.TeleBot(data.get_token())
 
+    #распознование команды /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
+        #отправка сообщения пользователю
     bot.send_message(message.chat.id, 'Приветствую')
+        #получение данных о пользователе
     user_id = message.chat.id
     username = str(message.chat.username)
     firstname = str(message.chat.first_name)
     secondname = str(message.chat.last_name)
-    #добавление пользователя в базу данных
+        #добавление пользователя в базу данных
     bd_def.add_user(user_id,username,firstname,secondname)
 
-
+    #распознование текстового сообщения
 @bot.message_handler(content_types=['text'])
 def send_message(message):
     bot.send_message(message.chat.id, 'Kekс')
+        #вывод сообщения в консоль pycharm
     print(message.chat.username + ' : ' + message.text )
+        #отправка сообщения Саше о том, что боту кто-то написал
     bot.send_message(data.sasha_chat_id(),message.chat.username + ' : ' + message.text)
 
+    #это метод, чтобы бот не завершался
 bot.polling()
